654d8d7c5ed348872a83e3e8c29385c135940981
Om Kadu <kaduom444@gmail.com>
2026-04-01 00:26:20 +0530

    hosted ready

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,6 @@
 const { startServer } = require("./src/server");
 
 startServer();
+
+// mongodb+srv://kaduom444:<pass>@cluster0.tnkcwdo.mongodb.net/?appName=Cluster0
+//  https://chatbloc-backend.onrender.com/
